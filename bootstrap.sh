d896b545aaa71e340394453b4fc1f2c4e23c67e4
Jack Cassidy <j.cassidy45@gmail.com>
2020-04-11 21:12:34 +0100

    add: docker-compose config from old homelab repo

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p $HOME/pihole && mkdir -p $HOME/dnsmasq.d
+sudo cp $HOME/homelab-hogwarts/lan.list $HOME/pihole/lan.list
+sudo cp $HOME/homelab-hogwarts/02-lan.list $HOME/dnsmasq.d/02-lan.list
